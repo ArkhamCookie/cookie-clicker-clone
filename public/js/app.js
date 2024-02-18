7ec17bdb9e1b6fdf55468893a57003250b80b4fb
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-02-18 06:52:08 -0600

    Add buying functionality for clickers

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,18 @@ cookieButton.addEventListener('click', function() {
 	currentCookies.textContent = player.cookies
 	totalCookies.textContent = player.stats.total
 })
+
+// Clicker Upgrade
+const clickerBuyButton = document.getElementById('buyClickerButton')
+const clickerAmountDisplay = document.querySelector('#currentClickers')
+let clickerAmount = 0
+
+clickerBuyButton.addEventListener('click', function() {
+	if (player.cookies >= 5) {
+		clickerAmount++
+		clickerAmountDisplay.textContent = clickerAmount
+
+		player.spend(5)
+		currentCookies.textContent = player.cookies
+	}
+})
